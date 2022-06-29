1c380b7de6f1a0aedcf2d930ae0728768f147feb
Davi <davioliveira-dev@outlook.com>
2022-06-29 10:20:25 -0300

    chore: setuping jest for tests

---
+++ server/__test__/user/user.test.ts
@@ -0,0 +1,10 @@
+test('List all users', async () => {
+  // list all
+  // list all with orderBy
+  // list one (id not found, cpf not found, email found)
+  // create one (email fail, cpf fail)
+  // update one (email fail, cpf fail)
+  // delete one (id not found)
+
+  it('should return a list of users', async () => {});
+});
